d58d380dd1067ae6686681057554a10d0a133186
unknown <you@example.com>
2023-03-20 23:08:16 +0700

    add

---
+++ ss4-AngularComponent &Template/Exercise/src/app/component/create-form/create-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+import {group} from "@angular/animations";
+import {retry} from "rxjs/operators";
+
+@Component({
+  selector: 'app-create-form',
+  templateUrl: './create-form.component.html',
+  styleUrls: ['./create-form.component.css']
+})
+export class CreateFormComponent implements OnInit {
+  createForm: FormGroup;
+
+  constructor() {
+    this.createForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.pattern("^\\+84\\d{9,10}$")])
+    }, {validators:this.checkConfirmPass})
+  }
+
+  checkConfirmPass(control: AbstractControl): any {
+      let confirmPassword = control.value;
+      let password = this.createForm.controls.confirmPassword.value;
+      return confirmPassword === password ? null : {notSame: true}
+  }
+
+  ngOnInit(): void {
+  }
+
+  save() {
+    if (this.createForm.valid) {
+      console.log(this.createForm.value);
+    }
+  }
+}
